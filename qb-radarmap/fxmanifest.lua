ff67bd0cfe1a9fb8dd0979cbe1b9016d2a2b86ab
Seaqn <80810882+Seaqn@users.noreply.github.com>
2021-10-04 01:57:51 +1030

    Add files via upload

---
+++ qb-radarmap/fxmanifest.lua
@@ -0,0 +1,17 @@
+--  ___ __  __ ___ _  _ ___ _  _  ___ ___   ___ _____ _   _ ___ ___ ___  ___ 
+-- | __|  \/  |_ _| \| | __| \| |/ __| __| / __|_   _| | | |   \_ _/ _ \/ __|
+-- | _|| |\/| || || .` | _|| .` | (__| _|  \__ \ | | | |_| | |) | | (_) \__ \
+-- |___|_|  |_|___|_|\_|___|_|\_|\___|___| |___/ |_|  \___/|___/___\___/|___/
+--                     https://github.com/orgs/Eminence-Studios
+
+fx_version 'bodacious'
+games { 'gta5' }
+
+client_script 'client.lua'
+server_script 'server.lua'
+
+files {
+	"hud.html"
+}
+
+ui_page "hud.html"
